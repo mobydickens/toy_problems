0cb88d6d2bc2cec4351916e2fa57537786c6405e
mobydickens <steadmegan@gmail.com>
2019-03-05 10:55:00 -0700

    peristent bugger level 6

--- level_6_katas.js
+++ level_6_katas.js
@@ -19,3 +19,14 @@ function solution(number){
   }
 }
 
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+function persistence(num) {
+  let howMany = 0
+  let number = num;
+  while(number > 9) {
+    howMany += 1;
+    let split = number.toString().split("");
+    number = split.reduce((a,b) => a * b);
+  }
+  return howMany;
+}
